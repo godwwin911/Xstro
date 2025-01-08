ce015e42e41cc0c036874cf0d4512f14f171cf79
godwwin911 <godwinmike911@gmail.com>
2025-01-08 21:41:33 +0100

    Update config.js

--- config.js
+++ config.js
@@ -2,11 +2,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const config = {
-	SESSION_ID: process.env.SESSION_ID || '',
-	SUDO: process.env.SUDO || '',
+	SESSION_ID: process.env.SESSION_ID || 'XSTRO_25_54_57',
+	SUDO: process.env.SUDO || '.',
 	API_ID: process.env.API_ID || 'https://xstro-api-4fb28ece11a9.herokuapp.com',
-	BOT_INFO: process.env.BOT_INFO || 'αѕтяσχ11;χѕтяσ м∂',
-	STICKER_PACK: process.env.STICKER_PACK || 'мα∂є ву;χѕтяσ мυℓтι ∂єνι¢є вσт',
+	BOT_INFO: process.env.BOT_INFO || 'GODWIN',
+	STICKER_PACK: process.env.STICKER_PACK || 'мα∂є ву;GODWIN',
 	WARN_COUNT: process.env.WARN_COUNT || 3,
 	TIME_ZONE: process.env.TIME_ZONE || 'Africa/Lagos',
 	DEBUG: process.env.DEBUG || false,
